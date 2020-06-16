92244f3bf4a1c91f8913c2ceca9b6648215ec7b2
Mackenzie Clark <clark.rmackenzie@gmail.com>
2020-06-16 00:52:49 -0400

    sidebar navigation working and responsive

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ document.getElementById("foodPage").style.display = "none";
 document.getElementById("drinkPage").style.display = "none";
 document.getElementById("userPage").style.display = "none";
 
-// SHOW HOME PAGE
-$("#home").on("click", showHome);
+// // SHOW HOME PAGE
 // id="home" button
 // id="homePage"
+$("#home").on("click", showHome);
 function showHome(){
     console.log("Enter showHome");
     document.getElementById("homePage").style.display = "block";
@@ -22,56 +22,54 @@ function showHome(){
 // id="foodPage"
 $("#food").on("click", showFoodPage);
 function showFoodPage(){
-    console.log("Enter showFoodPage");
     document.getElementById("homePage").style.display = "none";
     document.getElementById("foodPage").style.display = "block";
     document.getElementById("drinkPage").style.display = "none";
     document.getElementById("userPage").style.display = "none";
+ };
 
-};
+// // Listener for Meal Search
+// $("#searchMeal").on("click", mealList);
 
-// Listener for Meal Search
-$("#searchMeal").on("click", mealList);
+// // API call to retrieve Receipe Name, instruction, pic, quantity
+// function mealList(event) {
+//     //get user meal input
+//     var mealChoice = $("#mealInput").val();
+//     console.log("User input", mealChoice);
 
-// API call to retrieve Receipe Name, instruction, pic, quantity
-function mealList(event) {
-    //get user meal input
-    var mealChoice = $("#mealInput").val();
-    console.log("User input", mealChoice);
-
-    var fURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + mealChoice; 
+//     var fURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + mealChoice; 
     
-    $.ajax ({
-        url:fURL,
-        method:"GET"
-    }).then(processData)
+//     $.ajax ({
+//         url:fURL,
+//         method:"GET"
+//     }).then(processData)
 
 
-} // end mealList
+// } // end mealList
 
 
-function processData(fObject) {
-    console.log("food object is:", fObject);
-    var foodName = fObject.meals[0].strMeal;
-    var foodInst = fObject.meals[0].strInstructions;
+// function processData(fObject) {
+//     console.log("food object is:", fObject);
+//     var foodName = fObject.meals[0].strMeal;
+//     var foodInst = fObject.meals[0].strInstructions;
 
-    // loop thru max 20 ingredients
-    var index = 1;
-    abc = "strIngredient"+ index;
-    while (fObject.meals[0].("strIngredient"+index)) {
-        console.log("Success");
-        if ((fObject.meals[0].("strIngredient" + index) =="")) {
-            index++;
-            console.log("food ingredit");
-        } // end If
-    } // end While
+//     // loop thru max 20 ingredients
+//     var index = 1;
+//     abc = "strIngredient"+ index;
+//     while (fObject.meals[0].("strIngredient"+index)) {
+//         console.log("Success");
+//         if ((fObject.meals[0].("strIngredient" + index) =="")) {
+//             index++;
+//             console.log("food ingredit");
+//         } // end If
+//     } // end While
    
-    var foodIngre 
-    console.log("meal name:", foodName);
-    console.log("meal name:", foodInst);
+//     var foodIngre 
+//     console.log("meal name:", foodName);
+//     console.log("meal name:", foodInst);
 
-    // After received results, call mealListRender() for each one
-}
+//     // After received results, call mealListRender() for each one
+// }
 
 
 
@@ -79,11 +77,7 @@ function processData(fObject) {
 // id="drink" button
 // id="drinkPage"
 $("#drink").on("click", showDrinkPage);
-function showDrinkPage(event){
-    event.preventDefault();
-    event.stopPropagation();
-
-    console.log("Enter showDrinkPage");
+function showDrinkPage(){
     document.getElementById("homePage").style.display = "none";
     document.getElementById("foodPage").style.display = "none";
     document.getElementById("drinkPage").style.display = "block";
